70b57fe595ac97983888ad33efd689c7ca5681ee
thinkbell <kentabell0725@gmail.com>
2020-02-22 23:41:53 +0900

    update

--- src/02_example.py
+++ src/02_example.py
@@ -1,12 +1,17 @@
-from enum import Enum
 import random
+from enum import Enum
+from collections import deque
+
 import numpy as np
 import pyxel
+
+from map import Map
 
 class State(Enum):
   START = 1 # 開始演出
   MAIN = 2 # メイン
   CHANGE = 3 # マップ切り替え
+  END = 4 # 終了
 
 class Vehicle:
     def __init__(self, _x, _y, _c):
@@ -19,232 +24,44 @@ class Vehicle:
         self.x = _x
         self.y = _y
 
-class Map:
-    def __init__(self, w, h):
-        super().__init__()
-        self.w = w
-        self.h = h
-        self.data = np.zeros((h, w), np.int)
-        self.goal_x = 0 
-        self.goal_y = 0 
-        
-    def create_map_stick_down(self):
-        
-        # 柱の設置
-        for i in range(1, self.w)[::2]:
-            for j in range(1, self.h)[::2]:
-                self.data[j, i] = 1 # occupied
-                
-        # 柱を倒す
-        for i in range(1, self.w)[::2]:
-            for j in range(1, self.h)[::2]:
-                num = np.random.randint(3) # [r,u,l,d] = [0, 1, 2, 3]
-                if num == 0:
-                    self.data[j, i+1] = 1
-                elif num == 1:
-                    self.data[j-1, i] = 1
-                elif num ==2:
-                    self.data[j, i-1] = 1
-                elif num==3:
-                    self.data[j+1, i] = 1
-                    
-    def create_map_dungeon(self, num_row_rooms=2, num_col_rooms=3):
-        """
-        TODO: 部屋を作らなば場合も実する。その場合、全部がつながっているかをチェックする関数をつくる
-        """
-
-        self.data = np.ones(self.data.shape, np.int)
-        
-        # 適当に配列を分割する
-        # NOTE: 各値は偶数でないと道がつながらない。
-        rand_col_idx = [ int(e)&~1 for e in np.linspace(0, self.w-1, num=int(num_col_rooms)+1)]
-        rand_row_idx = [ int(e)&~1 for e in np.linspace(0, self.h-1, num=int(num_row_rooms)+1).astype(np.int)]
-
-        # 各部屋の中心, 大きさ
-        num_col_rooms = len(rand_col_idx) - 1
-        num_row_rooms = len(rand_row_idx) - 1
-        room_max_size_x = np.zeros((num_row_rooms, num_col_rooms),np.int)
-        room_max_size_y = np.zeros((num_row_rooms, num_col_rooms),np.int)
-        room_center_x = np.zeros((num_row_rooms, num_col_rooms), np.int)
-        room_center_y = np.zeros((num_row_rooms, num_col_rooms),np.int)
-
-        for ic in range(len(rand_col_idx)-1):
-            for ir in range(len(rand_row_idx)-1):
-                x = int(0.5 * (rand_col_idx[ic] + rand_col_idx[ic+1]))
-                size_x = rand_col_idx[ic+1] - rand_col_idx[ic]
-                y = int(0.5 * (rand_row_idx[ir] + rand_row_idx[ir+1]))
-                size_y = rand_row_idx[ir+1] - rand_row_idx[ir]
-
-                room_center_y[ir, ic] = int(y)
-                room_max_size_y[ir, ic] = int(size_y)
-                room_center_x[ir, ic] = int(x)
-                room_max_size_x[ir, ic] = int(size_x)
-        
-        # 各部屋の大きさを決める
-        room_size_x = np.zeros((num_row_rooms, num_col_rooms))
-        room_size_y = np.zeros((num_row_rooms, num_col_rooms))
-        for iy in range(num_row_rooms):
-            for ix in range(num_col_rooms):            
-                rmsx = room_max_size_x[iy,ix]
-                rmsy = room_max_size_y[iy,ix]
-                
-                rsx = np.random.randint(int(rmsx*0.3), int(rmsx*0.9))
-                rsy = np.random.randint(int(rmsy*0.3), int(rmsy*0.9))
-
-                room_size_x[iy, ix] = rsx
-                room_size_y[iy, ix] = rsy
-    
-        # 各部屋を塗りつぶす + 通路をつくる
-        for iy in range(num_row_rooms):
-            for ix in range(num_col_rooms):            
-                w = room_size_x[iy, ix]
-                h = room_size_y[iy, ix]
-                cx = room_center_x[iy, ix]
-                cy = room_center_y[iy, ix]                
-                rmsx = room_max_size_x[iy,ix]
-                rmsy = room_max_size_y[iy,ix]
-                self.data[cy-int(h/2.0):cy+int(h/2.0), cx-int(w/2.0):cx+int(w/2.0)] = 0
-        
-                # 各部かから通路への垂線を上下左右４本分生成
-                exit_x_up = np.random.randint(cx-int(w/2.0)+1, cx+int(w/2.0)-1)
-                exit_x_down = np.random.randint(cx-int(w/2.0)+1, cx+int(w/2.0)-1)
-                exit_left = np.random.randint(cy-int(h/2.0)+1, cy+int(h/2.0)-1)
-                exit_right = np.random.randint(cy-int(h/2.0)+1, cy+int(h/2.0)-1)
-                
-                # cxからcx+rmsx/2.0, cx-rmsx/2.0だけ線を引く
-                # ただし, 端に触れていへ部屋は通路をつくらない
-                
-                rx = int(rmsx/2.0)
-                ry = int(rmsy/2.0)
-                
-                if iy == 0 and ix == 0:
-                    # self.data[exit_left, cx-rx:cx] = 0
-                    self.data[exit_right, cx:cx+rx] = 0
-                    # self.data[cy-ry:cy, exit_x_up] = 0
-                    self.data[cy:cy+ry, exit_x_down] = 0
-
-                elif iy == num_row_rooms-1 and ix == num_col_rooms-1:
-                    self.data[exit_left, cx-rx:cx] = 0
-                    # self.data[exit_right, cx:cx+rx] = 0
-                    self.data[cy-ry:cy, exit_x_up] = 0
-                    # self.data[cy:cy+ry, exit_x_down] = 0
-
-                elif iy == 0 and ix == num_col_rooms-1:
-                    self.data[exit_left, cx-rx:cx] = 0
-                    # self.data[exit_right, cx:cx+rx] = 0
-                    # self.data[cy-ry:cy, exit_x_up] = 0
-                    self.data[cy:cy+ry, exit_x_down] = 0
-
-                elif iy == num_row_rooms-1 and ix == 0:
-                    # self.data[exit_left, cx-rx:cx] = 0
-                    self.data[exit_right, cx:cx+rx] = 0
-                    self.data[cy-ry:cy, exit_x_up] = 0
-                    # self.data[cy:cy+ry, exit_x_down] = 0
-
-                elif iy == num_row_rooms-1 and ix == 0:
-                    # self.data[exit_left, cx-rx:cx] = 0
-                    self.data[exit_right, cx:cx+rx] = 0
-                    self.data[cy-ry:cy, exit_x_up] = 0
-                    # self.data[cy:cy+ry, exit_x_down] = 0
-
-                elif iy == 0:
-                    self.data[exit_left, cx-rx:cx] = 0
-                    self.data[exit_right, cx:cx+rx] = 0
-                    # self.data[cy-ry:cy, exit_x_up] = 0
-                    self.data[cy:cy+ry, exit_x_down] = 0
-
-                elif iy == num_row_rooms-1:
-                    self.data[exit_left, cx-rx:cx] = 0
-                    self.data[exit_right, cx:cx+rx] = 0
-                    self.data[cy-ry:cy, exit_x_up] = 0
-                    # self.data[cy:cy+ry, exit_x_down] = 0
-
-                elif ix == 0:
-                    # self.data[exit_left, cx-rx:cx] = 0
-                    self.data[exit_right, cx:cx+rx] = 0
-                    self.data[cy-ry:cy, exit_x_up] = 0
-                    self.data[cy:cy+ry, exit_x_down] = 0
-
-                elif ix == num_col_rooms-1:
-                    self.data[exit_left, cx-rx:cx] = 0
-                    # self.data[exit_right, cx:cx+rx] = 0
-                    self.data[cy-ry:cy, exit_x_up] = 0
-                    self.data[cy:cy+ry, exit_x_down] = 0
-                            
-                else:                
-                    self.data[exit_left, cx-rx:cx] = 0
-                    self.data[exit_right, cx:cx+rx] = 0
-                    self.data[cy-ry:cy, exit_x_up] = 0
-                    self.data[cy:cy+ry, exit_x_down] = 0
-
-        # 線をつなげる
-        rand_col_idx = np.array(rand_col_idx)
-        # print(rand_col_idx)
-        for col_idx in rand_col_idx[1:-1]:
-            print(col_idx)
-            end_y_l = np.where(self.data[:, col_idx - 1]== 0 )
-            end_y_r = np.where(self.data[:, col_idx + 1]== 0 )
-            for idx in range(num_row_rooms):
-                end_y = sorted([end_y_l[0][idx], end_y_r[0][idx]])
-                self.data[end_y[0]:end_y[1]+1, col_idx] = 0                
-
-        rand_row_idx = np.array(rand_row_idx)
-        # print(rand_row_idx)
-        for row_idx in rand_row_idx[1:-1]:
-            end_x_u = np.where(self.data[row_idx - 1, :]== 0 )
-            end_x_d = np.where(self.data[row_idx + 1, :]== 0 )
-            for idx in range(num_col_rooms):
-                end_x = sorted([end_x_u[0][idx], end_x_d[0][idx]])
-                self.data[row_idx, end_x[0]:end_x[1]+1] = 0                
-
-        # ゴールの初期をきめておく
-        x = np.random.randint(self.w)
-        y = np.random.randint(self.h)        
-        while self.data[y, x] != 0:
-            x = np.random.randint(self.w)
-            y = np.random.randint(self.h)        
-        self.goal_x = x
-        self.goal_y = y
-
-    def set_goal(self):
-        # ゴールの設置 # TODO: 壁からは選ばないようにする
-        self.data[self.goal_y, self.goal_x] = 0 # Reset Goal
-
-        x = np.random.randint(self.w)
-        y = np.random.randint(self.h)        
-        while self.data[y, x] != 0:
-            x = np.random.randint(self.w)
-            y = np.random.randint(self.h)        
-
-        self.data[y, x] = -1 # Goal
-        self.goal_x = x
-        self.goal_y = y
- 
-
 class App:
     def __init__(self):
 
         self.name = "02_example"
         
         self.state = State.START
-
-        pyxel.init(128, 96, caption=self.name, scale=4, fps=15)
-        pyxel.image(0).load(0, 0, "pyxel_logo_38x16.png") # opening
+        
+        pyxel.init(96, 64, caption=self.name, scale=6, fps=15)
+        # pyxel.image(0).load(0, 0, "pyxel_logo_38x16.png") # opening
         
         # 地図の初期化
         self.map = Map(pyxel.width, pyxel.height)
         # self.map.create_map_stick_down()
-        self.map.create_map_dungeon()
+        self.num_col_rooms = 4
+        self.num_row_rooms = 4
+        self.map.create_map_dungeon(num_col_rooms=self.num_col_rooms, num_row_rooms=self.num_row_rooms)
         self.map.set_goal()
         
-        # キャラの初期化
+        # 自キャラの初期化
         x = int(np.random.rand() * pyxel.width)
         y = int(np.random.rand() * pyxel.height)
         while self.map.data[y, x] != 0:
             x = int(np.random.rand() * pyxel.width)
             y = int(np.random.rand() * pyxel.height)            
         c = 11
-        self.v = Vehicle(x, y, c)
+        self.ego = Vehicle(x, y, c)
+
+        # 敵キャラの初期化
+        x = int(np.random.rand() * pyxel.width)
+        y = int(np.random.rand() * pyxel.height)
+        while self.map.data[y, x] != 0:
+            x = int(np.random.rand() * pyxel.width)
+            y = int(np.random.rand() * pyxel.height)            
+        c = 14
+        self.enemy = Vehicle(x, y, c)
+
+        self.route = deque()
+
 
         # 獲得アイテす数
         self.num_got_items = 0
@@ -260,21 +77,7 @@ class App:
             
             if (pyxel.btn(pyxel.KEY_S)):
                 self.state = State.MAIN
-                
-        elif self.state == State.MAIN: # メイン
-            
-            self.move_target(self.v)
-            
-            # ゴールに到達したかどうかを判定
-            if self.map.data[self.v.y, self.v.x] == -1:
-                self.num_got_items += 1
-                
-                # 獲得アイテムが閾い異常のとき次のstageへ
-                if self.num_got_items >= self.max_items:
-                    self.state = State.CHANGE
-                
-                self.map.set_goal()
-                
+
         elif self.state == State.CHANGE:
             if (pyxel.btn(pyxel.KEY_S)):
                 self.state = State.MAIN
@@ -283,11 +86,65 @@ class App:
                 self.max_items = 3 # これだけ獲得したら次の画面へ
 
                 print("Init Map")
-                self.map.create_map_dungeon()
+                self.map.create_map_dungeon(num_col_rooms=self.num_col_rooms, num_row_rooms=self.num_row_rooms)
+                self.map.set_goal()
+                
+                # キャラの初期化
+                x = int(np.random.rand() * pyxel.width)
+                y = int(np.random.rand() * pyxel.height)
+                while self.map.data[y, x] != 0:
+                    x = int(np.random.rand() * pyxel.width)
+                    y = int(np.random.rand() * pyxel.height)            
+                self.ego.x = x
+                self.ego.y = y
+
+                x = int(np.random.rand() * pyxel.width)
+                y = int(np.random.rand() * pyxel.height)
+                while self.map.data[y, x] != 0:
+                    x = int(np.random.rand() * pyxel.width)
+                    y = int(np.random.rand() * pyxel.height)            
+                self.enemy.x = x
+                self.enemy.y = y
+                self.route = deque()
+
+
+        elif self.state == State.MAIN: # メイン
+
+            # 自キャラの操作            
+            self.move_target(self.ego)
+
+            # 敵キャラの行動
+            self.act_enemy()
+
+            # ゴールに到達したかどうかを判定
+            if self.map.data[self.ego.y, self.ego.x] == -1:
+                self.num_got_items += 1
+                
+                # 獲得アイテムが閾い異常のとき次のstageへ
+                if self.num_got_items >= self.max_items:
+                    self.state = State.CHANGE
+                
                 self.map.set_goal()
 
+            if self.ego.x == self.enemy.x and self.ego.y == self.enemy.y:
+                self.state = State.END
 
+    def act_enemy(self):
+        
+        # 最短経路の更新
+        if not pyxel.frame_count % 8:
+        
+            self.route = self.map.search_shortest_path_dws((self.enemy.y, self.enemy.x), (self.ego.y, self.ego.x))
+            self.route = deque(self.route)
+            self.route.popleft() # 一つ目はstartなので捨てる
 
+        if len(self.route) > 0:
+            next_cell = self.route.popleft()
+            self.enemy.update(next_cell[1], next_cell[0])
+
+        else:
+            pass
+        
     def move_target(self, target):
         x = target.x
         y = target.y            
@@ -308,20 +165,36 @@ class App:
     def draw(self):
         if self.state == State.START:
             pyxel.cls(0)
-            pyxel.text(int(pyxel.width/4.0), int(pyxel.height/2.0), "1 PLAYER GAME",  7 )            
-            # pyxel.blt(0, 0, 0, 0, 0, 38, 16)
+            pyxel.text(int(pyxel.width/4.0), int(pyxel.height/2.0), "1 PLAYER GAME",  7)            
             
         elif self.state == State.MAIN:
             pyxel.cls(0)
             
-            # 迷路を描画
+            # 迷路
             self.draw_map()    
-            pyxel.rect(self.v.x, self.v.y, 1, 1, self.v.c)
+            
+            # 敵キャラのパス
+            self.draw_enemy_route()
+            
+            # 自キャラ
+            pyxel.rect(self.ego.x, self.ego.y, 1, 1, self.ego.c)
+            
+            # 敵キャラ
+            pyxel.rect(self.enemy.x, self.enemy.y, 1, 1, self.enemy.c)
             
         elif self.state == State.CHANGE:
             pyxel.cls(0)
-            pyxel.text(0, 0, "Next map(s)", pyxel.frame_count % 16)
-            pyxel.blt(30, 40, 0, 0, 0, 38, 16)
+            pyxel.text(int(pyxel.width/4.0), int(pyxel.height/2.0), "NEXT MAP",  7 )            
+
+        elif self.state == State.END:
+            pyxel.cls(0)
+            pyxel.text(int(pyxel.width/4.0), int(pyxel.height/2.0), "GAME OVER",  7 )            
+
+    def draw_enemy_route(self):
+        for cell in self.route:
+            x = cell[1]
+            y = cell[0]
+            pyxel.rect(x, y, 1, 1, 6)
 
             
     def draw_map(self):
